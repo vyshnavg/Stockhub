ebbd5012e10edcb241bb77e77e5b2cd1994a66e1
vyshnav <vyshnavxlr8@gmail.com>
2017-12-28 10:42:16 -0800

    check

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -55,19 +55,6 @@
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form> -->
             <ul class="nav navbar-nav navbar-right">
-                <!-- <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                </ul>
-                </li> -->
-
-                
                 <?php if(!$this->session->userdata('logged_in')) : ?>
                     <li class="<?php if ($title=="Login") {echo "active"; } else  {echo "noactive";} ?>"> <a href="<?php echo base_url(); ?>users/login">Login</a></li>
                     <li class="<?php if ($title=="Sign Up") {echo "active"; } else  {echo "noactive";} ?>"> <a href="<?php echo base_url(); ?>users/register">Register</a></li>
